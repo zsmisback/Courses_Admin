03b8bc8e365834c3d62fc1149c3dad788b2a8ecb
Zama <coolzama36@gmail.com>
2020-07-02 14:10:36 +0530

    s

--- footer_main.php
+++ footer_main.php
@@ -83,7 +83,7 @@
         <div class="col-md-12 text-center copyright">
           
           <p class="float-md-center headd"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Munshiji</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
           
         </div>
