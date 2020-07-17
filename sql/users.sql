ec78b4e43f27ca5de0830c1fbfb49a722a430523
Zama <coolzama36@gmail.com>
2020-07-17 21:25:45 +0530

    New user sql

--- sql/users.sql
+++ sql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2020 at 01:35 PM
+-- Generation Time: Jul 17, 2020 at 05:55 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -39,6 +39,7 @@ CREATE TABLE `users` (
   `user_about` varchar(255) NOT NULL,
   `user_image` varchar(255) NOT NULL,
   `user_lvl` int(11) NOT NULL,
+  `user_joined` datetime NOT NULL,
   `user_ban` int(11) NOT NULL,
   `user_unique` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -47,8 +48,11 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_name`, `user_contact`, `user_email_address`, `user_password`, `user_courses`, `user_certificates`, `user_about`, `user_image`, `user_lvl`, `user_ban`, `user_unique`) VALUES
-(20, 'Zee', '1234566786', 'resheil@gmail.com', '$2y$10$5qZeNAHEc0NOkd9BXFl9Betk/sumcmKULCUPu4o7T.2ptsfy6SQ8u', '', '', 'heyys', '.jpg', 1, 0, 'wiek3rjjps');
+INSERT INTO `users` (`user_id`, `user_name`, `user_contact`, `user_email_address`, `user_password`, `user_courses`, `user_certificates`, `user_about`, `user_image`, `user_lvl`, `user_joined`, `user_ban`, `user_unique`) VALUES
+(20, 'Zee', '1234566786', 'resheil@gmail.com', '$2y$10$5qZeNAHEc0NOkd9BXFl9Betk/sumcmKULCUPu4o7T.2ptsfy6SQ8u', '', '', 'heyys', '.jpg', 1, '0000-00-00 00:00:00', 0, 'wiek3rjjps'),
+(28, 'Zee', '9524321232', 'coolzama36@gmail.com', '$2y$10$MqjIg9oYC73crMqPhhNpOu7ip42nm0TtAPhrFLNFJ0/pMM6xLPkV6', '', '', 'hey', '', 0, '2020-07-10 16:32:47', 0, 'uprw3j3oha'),
+(29, 'Zama', '1234566786', 'coolzama36@gmail.comss', '$2y$10$IxCaDUo1NUdBguJg9JWkfO6XpoBT9EwdiG1fKujt/.JazPhOpYyzG', '', '', '', '', 0, '2020-07-15 15:08:49', 0, 'hujes314s1'),
+(30, 'Hey', '1234566786', 'coolzama36@gmail.comsss', '$2y$10$.zOGK0t9hGqT0bdaV3AfPuFwFOa5ok3bN5hsA1gq/WUat4Rmv0xDC', '', '', '', '', 0, '2020-07-17 21:21:36', 0, '2ypipwkw89');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +73,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
